46bcca59b0b9ba43d3274b567237b86d61ab5847
oj-srivastava <59896121+oj-srivastava@users.noreply.github.com>
2026-08-23 21:38:21 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder op = new StringBuilder();
+        for(String str : strs){
+            if(str.equals("")){
+                str = "#empty#";
+            }
+            op.append(str).append("#ojas#");
+        }
+        return op.toString();
+    }
+
+    public List<String> decode(String str) {
+        if(str.equals("#ojas#")){
+            return new ArrayList<String>(List.of(""));
+        }
+        String[] parts = str.split("#ojas#");
+        List<String> output = new ArrayList<String>();
+        for(String part: parts){
+            if(part.length() == 0){
+                continue;
+            }
+            if(part.equals("#empty#")){
+                output.add("");
+            } else {
+                output.add(part);
+            }
+        }
+
+        return output;
+    }
+}
