8bcf0b9016a259e8374938219b3ad8af64ff402c
oj-srivastava <59896121+oj-srivastava@users.noreply.github.com>
2026-08-31 21:43:44 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        Map<Character, Integer> c1 = new HashMap<>();
+        Map<Character, Integer> win = new HashMap<>();
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        for(char c : s1.toCharArray()){
+            c1.put(c, c1.getOrDefault(c, 0) + 1);
+        }
+        for(int i=0;i<s1.length()-1;i++){
+            char c = s2.charAt(i);
+            win.put(c, win.getOrDefault(c, 0) + 1);
+        }
+        int l=0,r=s1.length()-1;
+        while(r<s2.length()){
+            char rc = s2.charAt(r);
+            win.put(rc, win.getOrDefault(rc, 0) + 1);
+            boolean res = true;
+            for(Map.Entry<Character, Integer> entry : c1.entrySet()){
+                if(win.containsKey(entry.getKey()) && win.get(entry.getKey()) >= entry.getValue()){
+                    continue;
+                } else {
+                    res = false;
+                    break;
+                }
+            }
+            // System.out.println("Returning loop true");
+            // System.out.println(l);
+            // System.out.println(r);
+            if(res) {return true;}
+            char lc = s2.charAt(l);
+            win.put(lc, win.get(lc) -1);
+            l++;r++;
+        }
+        // System.out.println("Returning final true");
+        return false;
+    }
+}
